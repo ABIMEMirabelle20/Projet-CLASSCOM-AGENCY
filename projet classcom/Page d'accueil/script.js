c838d92b7b4cb578e64a97f5b21cd4953ed7a98f
Nathan-Sasa <jonathanbikuta05@gmail.com>
2025-09-20 18:08:25 +0100

    bitton retour et nouvell fonctionnalite service

--- projet classcom/Page d'accueil/script.js
+++ projet classcom/Page d'accueil/script.js
@@ -8,3 +8,17 @@ btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('active')
     MobileNav.classList.toggle('active')
 })
+
+// noivelle fonctionnalité : section-cards
+const cards = document.querySelectorAll(".service-cards > .card")
+
+function cardActive(){
+    cards.forEach((items) =>{
+    items.classList.remove("active")
+    this.classList.add("active")
+    })
+}
+
+cards.forEach((items) =>{
+    items.addEventListener('click', cardActive)
+})
