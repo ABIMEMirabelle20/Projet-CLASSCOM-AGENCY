51b0baec41a5959c3e74ebd2d58aba4092150a45
Nathan-Sasa <jonathanbikuta05@gmail.com>
2025-09-24 21:15:24 +0100

    feat

--- projet classcom/Page d'accueil/script.js
+++ projet classcom/Page d'accueil/script.js
@@ -1,12 +1,39 @@
-// const body = document.body
-
+const body = document.body
 const btnMenu = document.querySelector(".btn-menu")
 const MobileNav = document.querySelector(".mobile-nav")
-
+const aboutTitle = document.querySelector(".about-title")
 
 btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('active')
     MobileNav.classList.toggle('active')
+})
+
+aboutTitle.style.opacity = 0
+// window.addEventListener('scroll', ()=>{
+    
+//     if(window.innerHeight + window.screenY >= document.body.offsetHeight){
+//         console.log("SCROLL BODY");
+//     }else{
+//         aboutTitle.style.opacity = 1
+//         console.log("BODY SCROLL");
+        
+//     }
+
+
+//     // if(window.scrollY > 650){
+//     //     aboutTitle.style.opacity = 1
+//     //     console.log("SCROLL");
+    
+//     // }
+
+// })
+window.addEventListener('scroll', ()=>{
+    if(aboutTitle.scrollIntoView){
+        aboutTitle.style.opacity = 1
+        aboutTitle.style.transition = "all " + .5+ "s "+ .3 + "s" +" ease-in"
+    }else if(!aboutTitle.scrollIntoView){
+        aboutTitle.style.opacity = 0
+    }
 })
 
 // noivelle fonctionnalité : section-cards
